ff1b39e55a308ace217e53f39ddff23a56166131
avengineer1226 <av.engineer1226@gmail.com>
2026-01-26 19:46:27 +0530

    change to Hackstreet logo

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,27 @@
 import React from "react";
 import Petals from "./components/Petals";
 import SamuraiBackground from "./components/SamuraiBackground";
-import SideScrollMenu from "./components/SideScrollMenu"; 
+import SideScrollMenu from "./components/SideScrollMenu";
 import CountdownTimer from "./components/CountdownTimer"; // <--- Import the timer
 import "./App.css";
 
 // Your logo
-import logoImg from "./assets/hackstreet_4.png"; 
+import logoImg from "./assets/hackstreet logo.png";
 
 function App() {
   return (
     <div className="main-wrapper">
-      
+
       {/* 1. Background Layer */}
       <SamuraiBackground />
       <Petals />
-      
+
       {/* 3. Center Content Layer */}
       <div className="content-layer">
-        
+
         {/* Logo */}
         <img src={logoImg} alt="HACKSTREET 4.0" className="hero-logo" />
-        
+
         {/* Timer added right here below the logo */}
         <CountdownTimer />
 
